99892076138587f24914318526ffc389e93e2fa5
The Badshah 24 <155899921+TheBadshah24@users.noreply.github.com>
2024-06-28 12:06:21 +0530

    Update human_readable.py

--- util/human_readable.py
+++ util/human_readable.py
@@ -3,10 +3,10 @@ def humanbytes(size):
     # 2**10 = 1024
     if not size:
         return ""
-    power = 5368709120
+    power = 3**10
     n = 0
     Dic_powerN = {0: ' ', 1: 'Ki', 2: 'Mi', 3: 'Gi', 4: 'Ti'}
     while size > power:
         size /= power
-        n += 5
-    return str(round(size, 5)) + " " + Dic_powerN[n] + 'B'
+        n += 1
+    return str(round(size, 3)) + " " + Dic_powerN[n] + 'B'
